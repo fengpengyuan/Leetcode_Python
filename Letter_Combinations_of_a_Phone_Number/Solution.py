2c3599f9152ab41c15a366ac82d40796a844dbfc
Fengpeng <fengiaw@163.com>
2016-04-29 21:05:27 -0400

    letter combinations-04/29/2016

--- Letter_Combinations_of_a_Phone_Number/Solution.py
+++ Letter_Combinations_of_a_Phone_Number/Solution.py
@@ -7,22 +7,20 @@ class Solution(object):
         :type digits: str
         :rtype: List[str]
         """
+        if not digits:
+            return []
         res = []
-        self.dfs(digits, "", res, 0)
+        dic = {0: "", 1: "", 2: "abc", 3: "def", 4: "ghi", 5: "jkl", 6: "mno", 7: "pqrs", 8: "tuv", 9: "wzyx"}
+        self.dfs(digits, dic, "", res, 0)
         return res
 
-    def dfs(self, digits, sol, res, cur):
+    def dfs(self, digits, dic, sol, res, cur):
         if cur == len(digits):
-            res.append(str(sol))
+            res.append(sol)
             return
-        s = self.getString(digits[cur] - '0')
+        s = dic[int(digits[cur])]
         for i in range(len(s)):
-            self.dfs(digits, sol + s[i], res, cur + 1)
+            self.dfs(digits, dic, sol + s[i], res, cur + 1)
 
 
-    def getString(self, i):
-        dic = {0: "", 1: "", 2: "abc", 3: "def", 4: "ghi", 5: "jkl", 6: "mno", 7: "pqrs", 8: "tuv", 9: "wzyx"}
-        return dic[i]
-
-
-print(Solution().letterCombinations("123"))
+print(Solution().letterCombinations("345"))
