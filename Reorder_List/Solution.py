6e870068da0d4c346f4835ffacea1b881a22d80a
Fengpeng <fengiaw@163.com>
2016-05-06 20:43:08 -0400

    day 7

--- Reorder_List/Solution.py
+++ Reorder_List/Solution.py
@@ -13,3 +13,56 @@ class Solution(object):
         :type head: ListNode
         :rtype: void Do not return anything, modify head in-place instead.
         """
+        if not head or not head.next:
+            return
+        fast, slow = head, head
+        # while fast and fast.next:
+        #     fast = fast.next.next
+        #     if not fast:
+        #         break
+        #     slow = slow.next
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+        secondHalf = slow.next
+        slow.next = None
+
+        secondHalf = self.reverseList(secondHalf)
+        cur1, cur2 = head, secondHalf
+        while cur1 and cur2:
+            pnext1 = cur1.next
+            pnext2 = cur2.next
+            cur1.next = cur2
+            cur2.next = pnext1
+
+            cur1 = pnext1
+            cur2 = pnext2
+
+
+    def reverseList(self, head):
+        if not head or not head.next:
+            return head
+        pre, cur = None, head
+
+        while cur:
+            pnext = cur.next
+            cur.next = pre
+            pre = cur
+            cur = pnext
+        return pre
+
+
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(3)
+head.next.next.next = ListNode(4)
+head.next.next.next.next = ListNode(5)
+head.next.next.next.next.next = ListNode(6)
+head.next.next.next.next.next.next = ListNode(7)
+
+
+Solution().reorderList(head)
+
+while head:
+    print head.val,
+    head=head.next
