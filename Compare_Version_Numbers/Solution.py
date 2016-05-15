57b83d552b143068a654addff6903564aac0be4d
Fengpeng <fengiaw@163.com>
2016-05-14 23:19:00 -0400

    day 12

--- Compare_Version_Numbers/Solution.py
+++ Compare_Version_Numbers/Solution.py
@@ -32,17 +32,18 @@ class Solution(object):
         """
         vs1 = version1.split('.')
         vs2 = version2.split('.')
-        if len(vs1) > len(vs2):
-            vs2 += ['0'] * (len(vs1) - len(vs2))
-        if len(vs2) > len(vs1):
-            vs1 += ['0'] * (len(vs2) - len(vs1))
-        i = 0
-        while i < len(vs1) and i < len(vs2):
-            if int(vs1[i]) < int(vs2[i]):
+        l = max(len(vs1), len(vs2))
+        for i in xrange(l):
+            num1 = 0
+            if i<len(vs1):
+                num1 = int(vs1[i])
+            num2 = 0
+            if i<len(vs2):
+                num2 = int(vs2[i])
+            if num1 < num2:
                 return -1
-            elif int(vs1[i]) > int(vs2[i]):
+            elif num1 > num2:
                 return 1
-            i += 1
         return 0
 
 print Solution().compareVersion("1", "1.0")
