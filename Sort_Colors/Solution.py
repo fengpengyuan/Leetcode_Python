b87ed2c365e8c63d0d3621b7eb3d543aaf6cc691
Fengpeng <fengiaw@163.com>
2016-05-23 08:54:11 -0400

    day 22

--- Sort_Colors/Solution.py
+++ Sort_Colors/Solution.py
@@ -14,17 +14,34 @@ class Solution(object):
             if nums[i] == 0:
                 i += 1
             elif nums[i] == 1:
-                nums[i] = nums[j]
-                nums[j] = 1
+                nums[i], nums[j] = nums[j], nums[i]
                 j -= 1
             else:
-                nums[i] = nums[k]
-                nums[k] = 2
+                nums[i], nums[k] = nums[k], nums[i]
                 k -= 1
                 if j > k:
                     j = k
 
-nums = [0,2,1,0,2,1,1,0]
+    def sortColors2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        i, j, k = 0, 0, len(nums) - 1
+        while i <= k:
+            if nums[i] == 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1
+                i += 1
+            elif nums[i] == 2:
+                nums[i], nums[k] = nums[k], nums[i]
+                k -= 1
+            else:
+                i += 1
+
+
+
+nums = [2, 0, 0, 0, 0]
 
 Solution().sortColors(nums)
 print nums
