af4d61799be66c7fcc7496f4ceb143e82a4226b6
Fengpeng <fengiaw@163.com>
2016-06-01 17:44:45 -0400

    day 30

--- Unique_Paths_II/Solution.py
+++ Unique_Paths_II/Solution.py
@@ -7,3 +7,15 @@ class Solution(object):
         :type obstacleGrid: List[List[int]]
         :rtype: int
         """
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        dp = [[0] * n for _ in xrange(m)]
+        dp[0][0] = 1 if obstacleGrid[0][0] == 0 else 0
+        for i in xrange(1, m):
+            dp[i][0] = dp[i - 1][0] if obstacleGrid[i][0] == 0 else 0
+        for j in xrange(1, n):
+            dp[0][j] = dp[0][j - 1] if obstacleGrid[0][j] == 0 else 0
+
+        for i in xrange(1, m):
+            for j in xrange(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] if obstacleGrid[i][j] == 0 else 0
+        return dp[m - 1][n - 1]
