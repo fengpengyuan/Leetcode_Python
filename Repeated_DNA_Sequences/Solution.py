4c383ac720901fdd2f0b66a95ef7d6fa12434ddc
Fengpeng <fengiaw@163.com>
2016-06-16 23:57:29 -0400

    day 39

--- Repeated_DNA_Sequences/Solution.py
+++ Repeated_DNA_Sequences/Solution.py
@@ -1,3 +1,5 @@
+import collections
+
 __author__ = 'fengpeng'
 
 
@@ -7,3 +9,33 @@ class Solution(object):
         :type s: str
         :rtype: List[str]
         """
+        res = []
+        dict = {}
+        for i in xrange(len(s) - 9):
+            seq = s[i:i + 10]
+            mask = self.encode(seq)
+            if mask in dict and dict[mask] == 1:
+                res.append(seq)
+                dict[mask] = 2
+            elif mask not in dict:
+                dict[mask] = 1
+        return res
+
+    def encode(self, s):
+        code = 0
+        for c in s:
+            code <<= 2
+            if c == 'A':
+                code += 0
+            elif c == 'C':
+                code += 1
+            elif c == 'T':
+                code += 2
+            else:
+                code += 3
+        return code
+
+
+print Solution().findRepeatedDnaSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT")
+
+
