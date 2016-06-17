0be16d3d3b5020c69450a20d4fb0cd981c481475
Fengpeng <fengiaw@163.com>
2016-06-17 16:24:59 -0400

    day 41

---
+++ Combinations/Solution.py
@@ -0,0 +1,10 @@
+__author__ = 'fengpeng'
+
+
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
